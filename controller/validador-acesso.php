4525ddfbc52247379459e1ec746d74263fce9d80
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2022-05-01 14:53:09 -0300

    correção de url no arquivo validador de acesso

--- controller/validador-acesso.php
+++ controller/validador-acesso.php
@@ -1,9 +1,9 @@
 <?php
    session_start();
    if(isset($_SESSION['autenticado']) && ($_SESSION['autenticado'] == 'nao')){
-    header('location: http://local.site/php/app-help-desk/index.php?login=erro');
+    header('location: http://localhost/meus-projetos/central-de-ajuda/index.php?login=erro');
   }
    if(!isset($_SESSION['autenticado']) && ($_SESSION['autenticado'] != 'sim')){
-      header('location: http://local.site/php/app-help-desk/index.php?login=erro2');
+      header('location: http://localhost/meus-projetos/central-de-ajuda/index.php?login=erro2');
    }
 ?>
